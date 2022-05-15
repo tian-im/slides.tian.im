24a39fb30125eefbbdc77ba0c3344ce3995a2a67
Tian Chen <me@tian.im>
2022-05-16 08:57:37 +1000

    feat: first slide example

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,3 @@
-function HomePage() {
-  return <div>Welcome to Next.js!</div>
+export default () => {
+  return <h1>Welcome to slides.tian.im!</h1>
 }
-
-export default HomePage
---
+++ pages/rails_performance.js
@@ -0,0 +1,12 @@
+import { Slide } from "components/Slide"
+
+export default () =>
+  (
+    <Slide>
+      <section>Horizontal Slide</section>
+      <section>
+        <section>Vertical Slide 1</section>
+        <section>Vertical Slide 2</section>
+      </section>
+    </Slide>
+  )
